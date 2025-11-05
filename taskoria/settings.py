050a8a49ff7554465d9292f630c2b2585d1f9178
laneloot <laneloot03@gmail.com>
2025-11-05 13:17:20 -0600

    chore: register core apps and DRF in settings

--- taskoria/settings.py
+++ taskoria/settings.py
@@ -37,6 +37,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # Third-party apps
+    'rest_framework',
+
+    # Taskoria apps
+    'users',
+    'projects',
+    'tasks',
+    'notifications',
+    'common',
 ]
 
 MIDDLEWARE = [
