46004365c2fdee9b6099203bd908642ee4c6eef0
laneloot <laneloot03@gmail.com>
2025-11-05 13:19:05 -0600

    feat: add versioned API URLs and app routing

--- taskoria/urls.py
+++ taskoria/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api/v1/users/', include('users.urls')),
+    path('api/v1/projects/', include('projects.urls')),
+    path('api/v1/tasks/', include('tasks.urls')),
+    path('api/v1/notifications/', include('notifications.urls')),
 ]
