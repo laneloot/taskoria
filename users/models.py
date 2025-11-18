8b69f81c5be93042840fb44e8697ee3f85403269
laneloot <laneloot03@gmail.com>
2025-11-18 08:39:18 -0600

    feat: add email_verified field to User model.

--- users/models.py
+++ users/models.py
@@ -10,6 +10,7 @@ class User(AbstractUser):
     )
 
     email = models.EmailField(unique=True)
+    email_verified = models.BooleanField(default=False)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="member")
     avatar = models.ImageField(upload_to="avatars/", null=True, blank=True)
     bio = models.TextField(blank=True)
